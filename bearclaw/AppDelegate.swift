8cb4f995d87ae8f5865fe0ae38821831642f3420
Xavier Decuyper <hi@savjee.be>
2017-11-22 08:08:42 +0100

    Initial Commit

---
+++ bearclaw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bearclaw
+//
+//  Created by Xavier Decuyper on 22/11/2017.
+//  Copyright © 2017 Savjee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ bearclaw/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bearclaw
+//
+//  Created by Xavier Decuyper on 22/11/2017.
+//  Copyright © 2017 Savjee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
